957b4dd2181075552136ad2e2be636595751a358
NetSe24 <netse0962@gmail.com>
2023-01-15 01:59:26 +0700

    style screen

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="htpps://unpkg.com/swiper@7/swiper-bundle.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title> Travel Home</title>
+</head>
+
+<body>
+    <section class="header">
+        <a href="#" class="logo"> Travel 🛬 </a>
+        <nav class="navbar">
+            <a href="index.php"> Home </a>
+            <a href="views/about.php"> About </a>
+            <a href="views/package.php"> Package </a>
+            <a href="views/book.php">  Book </a>
+        </nav>
+    <div id="menu-btn" class="fas fa-bars"></div>
+    
+    </section>
+
+
+</body>
+
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+<script src="js/script.js"></script>
+
+</html>
